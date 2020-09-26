5544de4594d1264a3e75ec6e1cb1b6669785aa0e
Łukasz Michalak <lukasz57570@gmail.com>
2020-09-26 16:20:41 +0200

    Added exports all current components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,12 @@
+export { default as AddButton } from './atoms/AddButton';
+export { default as Button } from './atoms/Button';
+export { default as ButtonLink } from './atoms/ButtonLink';
+export { default as SocialButton } from './atoms/SocialButton';
+export { default as ItemButton } from './atoms/ItemButton';
+export { default as Heading } from './atoms/Heading';
+export { default as Paragraph } from './atoms/Paragraph';
+export { default as Input } from './atoms/Input';
 export { default as Avatar } from './atoms/Avatar';
+export { default as Logo } from './atoms/Logo';
+
+export { default as NavBar } from './molecules/NavBar';
