de59b90b75352c125f39dd6b564362d9dd8cec44
Łukasz Michalak <lukasz57570@gmail.com>
2020-10-03 17:12:41 +0200

    Added redirect from HomePage to WordListPage

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import theme from 'theme/theme';
 import GlobalStyles from 'theme/GlobalStyles';
-import { LoginPage, HomePage } from 'views';
+import { LoginPage, UserPage } from 'views';
 import routes from 'routes';
-import UserPage from './UserPage';
 
 const Root = () => {
   return (
@@ -14,7 +13,9 @@ const Root = () => {
       <GlobalStyles />
       <Router>
         <Switch>
-          <Route exact path={routes.home} component={HomePage} />
+          <Route exact path={routes.home}>
+            <Redirect to={routes.wordsList} />
+          </Route>
           <Route path={routes.login} component={LoginPage} />
           <Route path={routes.wordsList} component={UserPage} />
           <Route path={routes.addWord} component={UserPage} />
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,6 @@
 export { default as Root } from './Root';
 export { default as LoginPage } from './LoginPage';
-export { default as HomePage } from './UserPage';
+export { default as UserPage } from './UserPage';
 export { default as WordsListPage } from './WordsListPage';
 export { default as AddWordPage } from './AddWordPage';
 export { default as GamePage } from './GamePage';
